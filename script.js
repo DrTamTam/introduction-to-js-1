8901a0c9fd7fceb94c6659b0270ac1ecbe825799
DrTamTam <102097588+DrTamTam@users.noreply.github.com>
2022-06-21 14:16:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("name").innerHTML = "Name: Halimat Shaibu";
+document.getElementById("height").innerHTML = "Height: 5 ft 5 inches";
+document.getElementById("country").innerHTML = "Country: Nigeria";
